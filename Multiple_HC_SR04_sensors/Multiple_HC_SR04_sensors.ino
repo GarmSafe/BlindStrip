c26e2deeb00bc77d02781910bb370ed0c20e0f31
unknown <maestroni.13099@PALEOCAPA.DID>
2018-01-11 10:14:41 +0100

    Multiple_HC_SR04_sensors.ino added (original)

--- Multiple_HC_SR04_sensors/Multiple_HC_SR04_sensors.ino
+++ Multiple_HC_SR04_sensors/Multiple_HC_SR04_sensors.ino
@@ -1,120 +1,217 @@
 #include <NewPing.h>
 
-#define SONAR_NUM 5 // Number of sensors.
-#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
-#define MAX_DISTANCE_2 400 // Maximum distance range (in cm).
-#define PING_INTERVAL 35 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
-#define PING_SPEED 250 // Ping frequency (in milliseconds), fastest we should ping is about 35ms per sensor
+/**
+*Number of sensors
+*/
+#define SONAR_NUM 5
+/**
+*Maximum distance (in cm) to ping.
+*/
+#define MAX_DISTANCE 100
+/**
+*Maximum distance range (in cm)
+*/
+#define MAX_DISTANCE_2 400
+/**
+*Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+*/
+#define PING_INTERVAL 35
+/**
+*Ping frequency (in milliseconds), fastest we should ping is about 35ms per sensor
+*/
+#define PING_SPEED 250
+/**
+*Holds the times when the next ping should happen for each sensor.
+*/
+unsigned long pingTimer[SONAR_NUM]; 
+/**
+*Where the ping distances are stored.
+*/
+unsigned int cm[SONAR_NUM];          
+/**
+*Keeps track of which sensor is active.
+*/
+uint8_t currentSensor = 0;         
+/**
+*distance from quadriceps to ground.
+*/
+int setup_value;  
+/**
+*Echo Pins of the ultrasonic sensors
+*/
+int echoPins[SONAR_NUM]={
+  A0,A1,A2,A3,A4};       
+  /**
+*vibrations
+*/
+int vibrations[SONAR_NUM]; 
+/**
+*Trigger pins of the ultrasonic sensors.
+*/
+int motPins[SONAR_NUM] = {
+  5,3,9,6,10};  
+  /**
+*Sensor object array.
 
-unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
-unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
-uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+*\param NewPing(2,A0,MAX_DISTANCE)  leg-sx (high)
+*\param NewPing(4,A1,MAX_DISTANCE)  leg-sx (low)
+*\param NewPing(7,A2,MAX_DISTANCE)  leg-dx (high) -> for holes, stairs
+*\param NewPing(8,A3,MAX_DISTANCE)  leg-dx (low)
+*\param NewPing(12,A4,MAX_DISTANCE) belt
 
-int echoPins[SONAR_NUM]={A0,A1,A2,A3,A4};       // Echo Pins of the ultrasonic sensors
+*/
+NewPing sonar[SONAR_NUM] = { 
+	
+//Each sensor's trigger pin, echo pin, and max distance to ping.
+	
+  NewPing(2, A0, MAX_DISTANCE), 
+  NewPing(4, A1, MAX_DISTANCE), 
+  NewPing(7, A2, MAX_DISTANCE), 
+  NewPing(8, A3, MAX_DISTANCE), 
+  NewPing(12, A4, MAX_DISTANCE) 
+  };  
 
-int vibrations[SONAR_NUM];  //vibrations
-int motPins[SONAR_NUM] = {3,5,6,9,10};  //Trigger pins of the vibration motors
-
-NewPing sonar[SONAR_NUM] = {   // Sensor object array.
-  NewPing(2, A0, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-  NewPing(4, A1, MAX_DISTANCE),
-  NewPing(7, A2, MAX_DISTANCE_2),
-  NewPing(8, A3, MAX_DISTANCE),
-  NewPing(12, A4, MAX_DISTANCE)
-};
-
+  /**
+*The setup() function is called when a sketch starts
+*/
 void setup() {
   Serial.begin(9600);
-  
-  pingTimer[0] = millis() + PING_SPEED;           // First ping starts at 100ms, gives time for the Arduino to chill before starting.
-  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
-  {
-    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
-  } 
+
+  set_ping_interval();
+  set_setup_value();
 }
+/**
+*The loop() function loops consecutively allowing your program to change and respond 
+*/
 
 void loop() {
-  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { //Loop through all the sensors.
     if (millis() >= pingTimer[i]) { 
       if(i == 0){
-        pingTimer[i] += PING_SPEED;     // Make sensor 1 fire again "PING_SPEED" second later
+        pingTimer[i] += PING_SPEED;  //Make sensor 1 fire again "PING_SPEED" ms later   
       }
       else{
-        pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;     // Make next sensor fire again "PING_INTERVAL" second after previous sensor fires
+        pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;    //Make next sensor fire again "PING_INTERVAL" second after previous sensor fires 
       }
-      
-      if(i == 0 && currentSensor == SONAR_NUM - 1){     // When all the sensors has pinged 
-        vibrate();  //vibrate function is called
-        printResults();  //printResults function is called
+
+      if(i == 0 && currentSensor == SONAR_NUM - 1){   //When all the sensors has pinged 
+        vibrate();
+        printResults();
       }
-      
-      currentSensor = i;                          // Sensor being accessed.
-      cm[currentSensor] = sonar[currentSensor].ping_cm();     // Send a ping, returns the distance in centimeters or 0 (zero) if no ping echo within set distance limit 
-      
-      if(currentSensor == 2)  //sensor that recognises ground's irregularities
+
+      currentSensor = i;  //Sensor being accessed.                 
+
+      cm[currentSensor] = sonar[currentSensor].ping_cm();      //Send a ping, returns the distance in centimeters or 0 (zero) if no ping echo within set distance limit
+
+      if(currentSensor == 2)  //sensor that recognises holes
       {
-        if(cm[currentSensor] == 0 || cm[currentSensor] > 75)  //if the no ping echo or the distance is higher than 75
+        if(cm[currentSensor] == 0 || (cm[currentSensor] - 10) > setup_value || (cm[currentSensor] + 10) < setup_value)  //if the no ping echo or the distance is higher than 75
         {
-          vibrations[currentSensor]=255;  //vibration
+          vibrations[currentSensor]=255; //vibrate
         }
         else
         {
-          vibrations[currentSensor]=0;  //no vibration
+          vibrations[currentSensor]=0;  //no vibrate
         }
       }
       else
       {
-        if(cm[currentSensor] == 0){     // if the no ping echo
-          reset();  // reset function is called
-          vibrations[currentSensor]=0;  //vibration = 0
+        if(cm[currentSensor] == 0){    //if the no ping echo
+          reset();
+          vibrations[currentSensor]=0;
         }
         else
         {
-          if(cm[currentSensor] == 1){  // adjust minimum range to 2cm
-          cm[currentSensor] = 2;
+          if(cm[currentSensor] == 1){   //adjust minimum range to 2cm
+            cm[currentSensor] = 2;
           }
-          
-          vibrations[currentSensor] = map(cm[currentSensor], 2, MAX_DISTANCE, 255, 80);  //calculate vibration according to the distance
+
+          vibrations[currentSensor] = map(cm[currentSensor], 2, MAX_DISTANCE, 255, 80); //calculate vibration according to the distance 
         }
       }
     }
   }
-  // Other code that *DOESN'T* analyze ping results can go here.
+ 
 }
 
-void reset(){     // This function is called to avoid the interruption of the sensor
+/**
+*The reset() function is called to avoid the interruption of the sensor
+*/
+void reset(){     
   pinMode( echoPins[currentSensor], OUTPUT );
   digitalWrite( echoPins[currentSensor], LOW );
   delayMicroseconds(10);
   pinMode( echoPins[currentSensor], INPUT); 
 }
 
-void printResults() { // Results are printed on the Serial monitor
+/**
+* the printResults() prints the results on the Serial monitor (used by developers)
+*/
+void printResults() { 
   Serial.print("- ");
-  
+
   for(uint8_t i = 0; i < SONAR_NUM; i++){
     Serial.print("SENSOR ");
     Serial.print(i);
     Serial.print(": ");
-    
-    if(cm[i] == 0){
-      Serial.print("RESET - ");
-    }
-    else{
-      Serial.print(cm[i]);
-      Serial.print("cm - "); 
-    }
-    
+
+    Serial.print(cm[i]);
+    Serial.print("cm - "); 
+
     Serial.print(vibrations[i]);
     Serial.print("vib - ");
   }
-  
+
   Serial.println();
   Serial.println();
 }
 
-void vibrate(){
+/**
+*the vibrate() function makes vibrate all the vibration motors with respective vibrations
+*/
+void vibrate(){ 
   for(uint8_t i=0; i < SONAR_NUM; i++){
-      analogWrite(motPins[i], vibrations[i]);
+    analogWrite(motPins[i], vibrations[i]);
   }
 }
+
+/**
+*the set_setup_value() function sets the setup_value on the basis of the height of the person
+*/
+
+void set_setup_value() { 
+  int sum=0,temp,;
+
+  for(int i=0;i<10;i++) // needed at least ten values
+  {
+    temp = sonar[2].ping_cm();  //send a ping
+
+    if(temp != 0) //if the ping is not out of range
+    {
+      sum += temp; //add the value to the sum
+    }
+    else
+    {
+      i--;  //ping another value
+    }
+    Serial.println();
+    Serial.println(temp);
+    delay(35);
+  }
+
+  setup_value = sum/10; //average of the ten values pinged
+  Serial.println(setup_value);
+}
+
+/**
+*the set_ping_interval() function sets the setup_value on the basis of the height of the person
+*/
+void set_ping_interval() {
+  pingTimer[0] = millis() + PING_SPEED; //First ping starts at "PING_SPEED" ms, gives time for the Arduino to chill before starting.
+
+  for (uint8_t i = 1; i < SONAR_NUM; i++)   //Set the starting time for each sensor.  
+  {
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+  }
+}
+
